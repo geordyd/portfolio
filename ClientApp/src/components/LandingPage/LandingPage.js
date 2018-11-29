86df9b1917ab6283a0148dee10e45e67728d1b5f
DESKTOP-6P67NTL\geord <geordycjd@gmail.com>
2018-11-29 17:31:18 +0100

    image added on landing page

--- ClientApp/src/components/LandingPage/LandingPage.js
+++ ClientApp/src/components/LandingPage/LandingPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './LandingPage.css';
 import { Element } from 'react-scroll';
+import { Image, Col, Row } from 'react-bootstrap';
 
 export class LandingPage extends Component {
   displayName = LandingPage.name
@@ -11,7 +12,12 @@ export class LandingPage extends Component {
         <Element name="LandingPage" className="element" >
           <div className="flex-container">
             <div>
-              <h1>Landing Page</h1>
+              <Col>
+                <Image src='https://via.placeholder.com/150' circle/>
+              </Col>
+              <div class="intro-name">Hello, I'm Geordy!</div>
+              <div class="tagline">Student Software Engineering</div>
+              
             </div>
           </div>
         </Element>
