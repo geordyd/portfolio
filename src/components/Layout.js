cd47d1e848bae77eab167d0562f2a6ab8259e6aa
geordyd <geordycjd@gmail.com>
2019-01-10 01:40:50 +0100

    Create a React app and publish it to GitHub Pages

---
+++ src/components/Layout.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Col, Grid, Row } from 'react-bootstrap';
+
+import { NavBar } from './NavBar/NavBar';
+import { LandingPage } from './LandingPage/LandingPage';
+import { About } from './About/About';
+import { PortfolioItems } from './PortfolioItems/PortfolioItems';
+
+export class Layout extends Component {
+  displayName = Layout.name
+
+  render() {
+    return (
+      <Grid fluid>
+        <Row> 
+
+          <NavBar />
+          
+          <LandingPage />
+          
+          <About />         
+
+          <PortfolioItems />
+
+          <Col sm={9}>
+            {this.props.children}
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
+}
