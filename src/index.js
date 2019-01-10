81804660722e971ca3e85c90434a71d9ae30f95d
geordyd <geordycjd@gmail.com>
2019-01-10 02:05:45 +0100

    .

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
@@ -11,9 +11,9 @@ const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
+  <HashRouter basename={baseUrl}>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   rootElement);
 
 registerServiceWorker();
