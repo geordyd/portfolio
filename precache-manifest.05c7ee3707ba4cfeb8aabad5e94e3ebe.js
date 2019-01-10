1cb1e82e6ce946d7dbc76c164608caccc22e85b7
geordyd <geordycjd@gmail.com>
2019-01-10 01:39:58 +0100

    Updates

---
+++ precache-manifest.05c7ee3707ba4cfeb8aabad5e94e3ebe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/portfolio/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "dd8132d16549ddd3f034",
+    "url": "/portfolio/static/css/main.806bf1fb.chunk.css"
+  },
+  {
+    "revision": "f721466883998665b87923b92dea655b",
+    "url": "/portfolio/static/media/glyphicons-halflings-regular.f7214668.svg"
+  },
+  {
+    "revision": "1c2a2f503ec39f594d24",
+    "url": "/portfolio/static/js/1.1c2a2f50.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "dd8132d16549ddd3f034",
+    "url": "/portfolio/static/js/main.dd8132d1.chunk.js"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/portfolio/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/portfolio/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/portfolio/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "1c2a2f503ec39f594d24",
+    "url": "/portfolio/static/css/1.78fdad7a.chunk.css"
+  },
+  {
+    "revision": "7d1971125f2ed986a6e8225d2f01d8fe",
+    "url": "/portfolio/index.html"
+  }
+];
