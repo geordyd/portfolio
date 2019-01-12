bcb1aa7195d75785d96d2dd50d90331c920093da
geordyd <geordycjd@gmail.com>
2019-01-12 17:36:46 +0100

    Updates

---
+++ precache-manifest.f485a6d7658bcbb42e8e6f3133057a95.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/portfolio/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "b54a77d7dc984ab609f5",
+    "url": "/portfolio/static/css/main.9fa0524d.chunk.css"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/portfolio/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "753a14ac5040e41686df",
+    "url": "/portfolio/static/js/1.753a14ac.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "dc20ac178accc8e615aaf3c6367c7d22",
+    "url": "/portfolio/static/media/Naamloos.dc20ac17.png"
+  },
+  {
+    "revision": "b54a77d7dc984ab609f5",
+    "url": "/portfolio/static/js/main.b54a77d7.chunk.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/portfolio/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "f721466883998665b87923b92dea655b",
+    "url": "/portfolio/static/media/glyphicons-halflings-regular.f7214668.svg"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/portfolio/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "753a14ac5040e41686df",
+    "url": "/portfolio/static/css/1.bc03d10a.chunk.css"
+  },
+  {
+    "revision": "f37f2580f9e6693f201121d7151c0871",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c4e755411a4c2c83d3777861a2beff86.js"
+  "/portfolio/precache-manifest.f485a6d7658bcbb42e8e6f3133057a95.js"
 );
 
 workbox.clientsClaim();
