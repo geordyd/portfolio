cc82e49e1f78b315108b07f16226d22beb238e85
geordyd <geordycjd@gmail.com>
2019-02-04 00:08:52 +0100

    Updates

---
+++ precache-manifest.452b8a55d056dbee8b3fe15ef609604f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc20ac178accc8e615aaf3c6367c7d22",
+    "url": "/portfolio/static/media/Naamloos.dc20ac17.png"
+  },
+  {
+    "revision": "f7736080cc19f472fa65",
+    "url": "/portfolio/static/css/main.c1245efe.chunk.css"
+  },
+  {
+    "revision": "5144e64367d5d3f523c5f1fd4df1133f",
+    "url": "/portfolio/static/media/webshop.5144e643.png"
+  },
+  {
+    "revision": "ea5997be3474fda07856",
+    "url": "/portfolio/static/js/1.ea5997be.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "f7736080cc19f472fa65",
+    "url": "/portfolio/static/js/main.f7736080.chunk.js"
+  },
+  {
+    "revision": "a56089c8fa572b7a1407026a03ddfbc5",
+    "url": "/portfolio/static/media/img2.a56089c8.png"
+  },
+  {
+    "revision": "37a5be8cb1aea2730e45b899d5dbc1d4",
+    "url": "/portfolio/static/media/background.37a5be8c.png"
+  },
+  {
+    "revision": "b070b87014074cd90caaa4fcf1f22ab5",
+    "url": "/portfolio/static/media/Trailer.b070b870.mp4"
+  },
+  {
+    "revision": "ea5997be3474fda07856",
+    "url": "/portfolio/static/css/1.dc7848b3.chunk.css"
+  },
+  {
+    "revision": "cdbcfe7d720b019bcf84625c8d2f970f",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.1421aa491daa47fb822871b9b5d0d258.js"
+  "/portfolio/precache-manifest.452b8a55d056dbee8b3fe15ef609604f.js"
 );
 
 workbox.clientsClaim();
