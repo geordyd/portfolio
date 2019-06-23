359cde971396007ae7da95107e46eba2662e0981
geordyd <geordycjd@gmail.com>
2019-06-23 22:33:18 +0200

    Updates

---
+++ precache-manifest.884d0396db7a5ee5d54b9af4a1fd9c97.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "5144e64367d5d3f523c5f1fd4df1133f",
+    "url": "/portfolio/static/media/webshop.5144e643.png"
+  },
+  {
+    "revision": "37a5be8cb1aea2730e45b899d5dbc1d4",
+    "url": "/portfolio/static/media/background.37a5be8c.png"
+  },
+  {
+    "revision": "3858f1cd58fffe61d1f605743245758e",
+    "url": "/portfolio/static/media/Trailer2.3858f1cd.mp4"
+  },
+  {
+    "revision": "b070b87014074cd90caaa4fcf1f22ab5",
+    "url": "/portfolio/static/media/Trailer.b070b870.mp4"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "932f6c8015dcd18c7962",
+    "url": "/portfolio/static/js/main.932f6c80.chunk.js"
+  },
+  {
+    "revision": "d92e7ad238ca03e7e56d",
+    "url": "/portfolio/static/js/1.d92e7ad2.chunk.js"
+  },
+  {
+    "revision": "932f6c8015dcd18c7962",
+    "url": "/portfolio/static/css/main.4686cc2e.chunk.css"
+  },
+  {
+    "revision": "d92e7ad238ca03e7e56d",
+    "url": "/portfolio/static/css/1.6934cbd5.chunk.css"
+  },
+  {
+    "revision": "44d3720f7c299a267825893a189d3c14",
+    "url": "/portfolio/index.html"
+  }
+];
