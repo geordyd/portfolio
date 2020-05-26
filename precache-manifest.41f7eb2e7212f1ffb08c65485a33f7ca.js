d29fbccccd318b8f9a7af80eb7f3ab77021823c8
Geordy <GeordyCJD@gmail.com>
2020-05-27 00:06:17 +0200

    Updates

---
+++ precache-manifest.41f7eb2e7212f1ffb08c65485a33f7ca.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a1b8c82b7a0bb0cebc409faaf142c35",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d7597bf9a231bdcb8ba5",
+    "url": "/portfolio/static/css/2.49a8fccd.chunk.css"
+  },
+  {
+    "revision": "a55ed3c02126d348d120",
+    "url": "/portfolio/static/css/main.2a68aea3.chunk.css"
+  },
+  {
+    "revision": "d7597bf9a231bdcb8ba5",
+    "url": "/portfolio/static/js/2.e276e55d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.e276e55d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a55ed3c02126d348d120",
+    "url": "/portfolio/static/js/main.fbf433b6.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "b070b87014074cd90caaa4fcf1f22ab5",
+    "url": "/portfolio/static/media/Trailer.b070b870.mp4"
+  },
+  {
+    "revision": "3858f1cd58fffe61d1f605743245758e",
+    "url": "/portfolio/static/media/Trailer2.3858f1cd.mp4"
+  },
+  {
+    "revision": "37a5be8cb1aea2730e45b899d5dbc1d4",
+    "url": "/portfolio/static/media/background.37a5be8c.png"
+  },
+  {
+    "revision": "5144e64367d5d3f523c5f1fd4df1133f",
+    "url": "/portfolio/static/media/webshop.5144e643.png"
+  }
+]);
