02b72fd65cdbb9b1608c140c3fd4b5be7f709579
Geordy <0935428@hr.nl>
2021-06-13 16:48:21 +0200

    Updates

---
+++ precache-manifest.42cc435cbe962ae2d48e43db774cd908.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e714a0f205fd573e2b7bd857ea7b1b25",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "36f9295f7d34e41215c0",
+    "url": "/portfolio/static/css/2.6ea84e4f.chunk.css"
+  },
+  {
+    "revision": "3560c82b550d9de70342",
+    "url": "/portfolio/static/css/main.eb0d03d0.chunk.css"
+  },
+  {
+    "revision": "36f9295f7d34e41215c0",
+    "url": "/portfolio/static/js/2.44cc6244.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/portfolio/static/js/2.44cc6244.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3560c82b550d9de70342",
+    "url": "/portfolio/static/js/main.8bc985fa.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "b070b87014074cd90caaa4fcf1f22ab5",
+    "url": "/portfolio/static/media/Trailer.b070b870.mp4"
+  },
+  {
+    "revision": "3858f1cd58fffe61d1f605743245758e",
+    "url": "/portfolio/static/media/Trailer2.3858f1cd.mp4"
+  },
+  {
+    "revision": "37a5be8cb1aea2730e45b899d5dbc1d4",
+    "url": "/portfolio/static/media/background.37a5be8c.png"
+  },
+  {
+    "revision": "ce0f0ca0378064f72f84a82140707e0c",
+    "url": "/portfolio/static/media/cultsense.ce0f0ca0.png"
+  },
+  {
+    "revision": "d36396bdd86d43be0a31c5bfeabd796b",
+    "url": "/portfolio/static/media/isd.d36396bd.png"
+  },
+  {
+    "revision": "58f2db80514d46b42284fdb11b2e0b1d",
+    "url": "/portfolio/static/media/minorai.58f2db80.png"
+  },
+  {
+    "revision": "5144e64367d5d3f523c5f1fd4df1133f",
+    "url": "/portfolio/static/media/webshop.5144e643.png"
+  }
+]);
